5bac3ff8757775ff2f990ab546e7757d4eb56541
董淑伟 <dsw0602@foxmail.com>
2018-07-20 15:59:17 +0800

    delete Initializable.sol

--- contracts/Upgradeable.sol
+++ contracts/Upgradeable.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.4.24;
 
 import './UpgradeabilityStorage.sol';
-import './Initializable.sol';
 
 /**
  * @title Upgradeable
  * @dev This contract holds all the minimum required functionality for a behavior to be upgradeable.
  * This means, required state variables for owned upgradeability purpose and simple initialization validation.
  */
-contract Upgradeable is UpgradeabilityStorage,Initializable {
+contract Upgradeable is UpgradeabilityStorage {
     /**
     * @dev Validates the caller is the versions registry.
     * THIS FUNCTION SHOULD BE OVERRIDDEN CALLING SUPER
     * @param sender representing the address deploying the initial behavior of the contract
     */
-    function initialize(address sender) isInitialized public payable {
+    function initialize(address sender) public payable {
         require(msg.sender == address(registry));
     }
 }
